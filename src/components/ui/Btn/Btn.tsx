2bc260adf866bed7cbc170569de9de02119a84c6
dima.rogkov <dimarogkov41475@ukr.net>
2024-05-24 15:36:17 +0300

    feature/add ui components

---
+++ src/components/ui/Btn/Btn.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+type Props = {
+    children: React.ReactNode;
+    href: string;
+    className?: string;
+};
+
+export const Btn: React.FC<Props> = ({ children, href, className = '' }) => {
+    return (
+        <Link
+            to={href}
+            className={`flex items-center justify-center sm:w-fit sm:min-w-[120px] lg:min-w-[140px] h-[40px] lg:h-[44px] lg:text-[18px] font-media tracking-[0.5px] rounded-[4px] text-[#fff] bg-[#22c55e] transition-opacity duration-300 hover:opacity-80 ${className}`}
+        >
+            {children}
+        </Link>
+    );
+};
